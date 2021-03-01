54c176c4c6c1e26580ce25488b76b932ca8ce54c
bachra <bachbrahem@gmail.cm>
2021-03-01 21:43:03 +0100

    ip

--- partie3/Client.java
+++ partie3/Client.java
@@ -69,7 +69,7 @@ public class Client {
     public static void main(String[] args) {
         try {
             //Registry registry = LocateRegistry.getRegistry(host);
-            stub = (IBanque)Naming.lookup("rmi://localhost:1900" + "/Banque");
+            stub = (IBanque)Naming.lookup("rmi://197.0.11.104:1900" + "/Banque");
             //stub = (IBanque) registry.lookup("Banque");
             boolean t = true;
             while (t) {
